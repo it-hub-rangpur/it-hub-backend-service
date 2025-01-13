89315ea1d21d69a0b482204c7bcda629b9bafa5a
sohag-arnifi <sohag.b@arnifi.com>
2025-01-13 14:45:48 +0600

    message create code updated

--- src/modules/messages/messages.router.ts
+++ src/modules/messages/messages.router.ts
@@ -3,9 +3,9 @@ import { messagesController } from "./messages.controller";
 
 const router = express.Router();
 router.route("/").get(messagesController.sendMessage);
+router.route("/:phone").get(messagesController.sendAutoOtp);
 router.route("/create").post(messagesController.create);
 router.route("/otp").post(messagesController.autoOtp);
 router.route("/get-all").get(messagesController.getAll);
-router.route("/:phone").post(messagesController.sendAutoOtp);
 
 export const messagesRouter = router;
