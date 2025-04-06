f4d8d2cd91012855ef14e69e5f094204e2fbfa4a
sohag-arnifi <sohag.b@arnifi.com>
2025-04-06 14:52:54 +0600

    crush - issue fixed

--- src/modules/server/utils/makeRequest.ts
+++ src/modules/server/utils/makeRequest.ts
@@ -1,71 +1,3 @@
-// import { Headers, ProxyAgent } from "undici";
-// import { socketIo } from "../../../socket";
-// import ApiError from "../../../errorHandelars/ApiError";
-
-// export const retryCodes = [403, 500, 502, 504, 419];
-
-// // const targetUrl = "http://localhost:5000/api/v1";
-// const targetUrl = "https://payment.ivacbd.com";
-// // const targetUrl = "https://www.google.com";
-// // const targetUrl = "https://cms.it-hub.agency";
-
-// export interface IMakeRequest {
-//   method: string;
-//   path: string;
-//   uri: string;
-//   cookies: string[];
-//   data?: any;
-// }
-
-// const makeRequest = async (
-//   requestInfo: IMakeRequest,
-//   _id: string,
-//   retries = 1
-// ): Promise<any> => {
-//   const client = new ProxyAgent({ uri: requestInfo.uri });
-//   const headers = new Headers();
-//   headers.set("accept-language", "en-US,en;q=0.9");
-//   headers.set("accept-encoding", "gzip, deflate, br");
-//   headers.set("connection", "keep-alive");
-//   headers.set("Accept", "application/x-www-form-urlencoded;charset=UTF-8;");
-//   headers.set(
-//     "Content-Type",
-//     "application/x-www-form-urlencoded;charset=UTF-8; application/json;"
-//   );
-
-//   const requestCookie = requestInfo?.cookies?.map((cookie) => {
-//     const cookieName = cookie.split("=")[0];
-//     const cookieValue = cookie.split(";")[0]?.split("=")[1];
-//     return `${cookieName}=${cookieValue}`;
-//   });
-
-//   headers.set("Cookie", requestCookie?.join("; "));
-//   const htmlResponse = await fetch(targetUrl + requestInfo?.path, {
-//     // dispatcher: client,
-//     // method: requestInfo?.method,
-//     // headers: headers,
-//     // body:
-//     //   requestInfo?.method !== "GET" ? JSON.stringify(requestInfo?.data) : null,
-//     // redirect: "manual",
-//   });
-//   const status = htmlResponse.status;
-//   if (retryCodes.includes(status) && retries > 0) {
-//     socketIo.emit("server-logs", {
-//       id: _id,
-//       log: {
-//         action: `Status:${status} | Retrying...(${retries})`,
-//         status: "Error",
-//         color: "error",
-//       },
-//     });
-//     await new Promise((resolve) => setTimeout(resolve, 1500));
-//     return makeRequest(requestInfo, _id, retries - 1);
-//   }
-//   return htmlResponse;
-// };
-
-// export default makeRequest;
-
 import { Headers, fetch, Agent, ProxyAgent } from "undici";
 import { socketIo } from "../../../socket";
 import ApiError from "../../../errorHandelars/ApiError";
@@ -87,12 +19,12 @@ const makeRequest = async (
   _id: string,
   retries = 1
 ): Promise<any> => {
-  // const httpsAgent = new Agent({
-  //   keepAliveTimeout: 300000,
-  //   keepAliveMaxTimeout: 1800000,
-  //   connections: 1000,
-  //   pipelining: 1,
-  // });
+  const httpsAgent = new Agent({
+    keepAliveTimeout: 300000,
+    keepAliveMaxTimeout: 1800000,
+    connections: 1000,
+    pipelining: 1,
+  });
 
   const client = new ProxyAgent({
     uri: requestInfo.uri,
@@ -122,7 +54,7 @@ const makeRequest = async (
 
   try {
     const htmlResponse = await fetch(targetUrl + requestInfo?.path, {
-      // dispatcher: client,
+      dispatcher: httpsAgent,
       method: requestInfo?.method,
       headers: headers,
       body:
@@ -146,7 +78,6 @@ const makeRequest = async (
       await new Promise((resolve) => setTimeout(resolve, 1500));
       return makeRequest(requestInfo, _id, retries - 1);
     }
-
     return htmlResponse;
   } catch (error) {
     console.error("Error making request:", error);
